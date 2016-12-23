6f465a20bc139606458f8b664834fc5c8b7733bb
Tyler Long <tyler4long@gmail.com>
2016-12-23 17:13:46 +0800

    Add folder async-action-creator

---
+++ async-action-creator/src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { connect } from 'react-redux';
+import {showNotification, hideNotification} from './actions';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.showNotificationWithTimeout = this.showNotificationWithTimeout.bind(this);
+  }
+
+  showNotificationWithTimeout(text) {
+    this.props.showNotification(text);
+    setTimeout(() => {
+      this.props.hideNotification();
+    }, 5000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          <button onClick={() => this.showNotificationWithTimeout('Hello')}>Show Hello</button>
+          &nbsp;
+          <button onClick={() => this.showNotificationWithTimeout('Async')}>Show Async</button>
+          <h1>{this.props.text}</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+App = connect((state) => {
+  return {
+    text: state.text,
+  };
+}, (dispatch) => {
+  return {
+    showNotification: (text) => {
+      dispatch(showNotification(text));
+    },
+    hideNotification: () => {
+      dispatch(hideNotification());
+    },
+  };
+})(App);
+
+
+export default App;
